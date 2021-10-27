7090c6660888a5492480a2f22b642c339b7a404f
Daniel <daniel.hope@sky.uk>
2021-10-27 11:25:35 +0100

    Small changes

--- tests/EScooterApp.test.js
+++ tests/EScooterApp.test.js
@@ -2,10 +2,33 @@ const ChargingStation = require("../src/ChargingStation");
 const EScooter = require("../src/EScooter");
 const User = require("../src/User")
 
+let StationN = new ChargingStation("North", 10)
+let StationS = new ChargingStation("South", 10)
 
+let EScooter1 = new EScooter(1, StationN)
+let EScooter2 = new EScooter(2, StationS)
+let EScooter3 = new EScooter(3, StationN)
+
+let newUser = new User("Daniel")
+
+EScooter1.use(50)
 
 describe("Charging Station", () => {
-    test("Should charge scooter to 100", () => {
-        expect(Windsor.charge(Scooter1)).toBe("string")
+    test("Should append Scooter", () => {
+        let tstation = new ChargingStation("tempst", 10)
+        let temp = new EScooter(5, tstation)
+        expect(tstation.scooters).toEqual([temp])
+    })
+})
+
+describe("E-Scooter", () => {
+    test("", () => {
+
+    })
+})
+
+describe("User", () => {
+    test("", () => {
+
     })
 })
